c4fd0e97d7243bca7487b3ad6459d1f24a761bde
Giedrius <stankauskas69@gmail.com>
2019-01-30 21:12:46 +0200

    php01

---
+++ index01.php
@@ -0,0 +1,12 @@
+<?php
+  $salis = 'Lietuva';
+  $miestas = 'Vilnius';
+  $populiacija = 574221;
+
+  echo
+    "<ul>
+      <li>$salis</li>
+      <li>$miestas</li>
+      <li>$populiacija</li>
+    </ul>";
+?>
