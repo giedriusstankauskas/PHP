e99770746a53a5f16992e9a396564aa47f14b905
Giedrius <stankauskas69@gmail.com>
2019-01-30 21:13:05 +0200

    php03

---
+++ index03.php
@@ -0,0 +1,19 @@
+<?php
+$x = 10;
+$y = 7;
+
+$result = $x + $y;
+echo "$x + $y = $result <br>";
+
+$result = $x - $y;
+echo "$x - $y = $result <br>";
+
+$result = $x * $y;
+echo "$x * $y = $result <br>";
+
+$result = $x / $y;
+echo "$x / $y = $result <br>";
+
+$result = $x % $y;
+echo "$x % $y = $result <br>";
+?>
